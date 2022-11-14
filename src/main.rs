1f168eb506e8c08e5e91b1f1054fab530de1faea
Korzhavin Ivan <korjavin@gmail.com>
2022-11-14 22:12:34 +0300

    Add prom

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+use axum_prometheus::PrometheusMetricLayer;
+
+#[tokio::main]
+async fn main() {
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/metrics", get(|| async move { metric_handle.render() }))
+        .route("/health", get(|| async { "OK"}))
+        .layer(prometheus_layer);
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
+
